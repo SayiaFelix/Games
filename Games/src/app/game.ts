39914a44e34b834e72d77e7f5c24e2e544e00607
SayiaFelix <sayiafelix18@gmail.com>
2022-09-23 09:35:23 +0300

    adding pipe

--- Games/src/app/game.ts
+++ Games/src/app/game.ts
@@ -2,7 +2,7 @@ export class Game {
 
     showDescription:boolean | undefined;
 
-    constructor(public id:number,public name:string,public description:string){
+    constructor(public id:number,public name:string,public description:string, public playDate:Date){
        this.showDescription=false;
     }
     // id: number | undefined;
--- Games/src/app/game/game.component.ts
+++ Games/src/app/game/game.component.ts
@@ -9,9 +9,9 @@ import { Game } from '../game';
 export class GameComponent implements OnInit {
 
   games:Game[]=[
-    new Game(1,'FIFA 22','Love the Graphics'),
-    new Game(2,'Pes 22','The speed is awesome'),
-    new Game(3,'Dream League','Compatible in my device')
+    new Game(1,'FIFA 22','Love the Graphics',new Date(2020,0,18)),
+    new Game(2,'Pes 22','The speed is awesome',new Date(2021,5,18)),
+    new Game(3,'Dream League','Compatible in my device',new Date(2022,8,29))
     // {id:1,name:'FIFA 22',description:'Love the graphics'},
     // {id:2,name:'PES 22',description:'The speed is awesome while using it'},
     // {id:3,name:'Dream League 2022',description:'Compatible in my device'}
