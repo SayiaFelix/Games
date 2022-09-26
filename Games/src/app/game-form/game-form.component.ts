d57addb6b809482b633036663fded2b31c7d33ea
SayiaFelix <sayiafelix18@gmail.com>
2022-09-26 11:52:17 +0300

    creating forms

--- Games/src/app/game-form/game-form.component.ts
+++ Games/src/app/game-form/game-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../game';
 
 @Component({
   selector: 'app-game-form',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-form.component.css']
 })
 export class GameFormComponent implements OnInit {
+
+  newGame = new Game (0,"","",new Date())
 
   constructor() { }
 
