182c4d4fc6c98fc8c70ca898df38f93dc5166a6c
SayiaFelix <sayiafelix18@gmail.com>
2022-09-27 09:50:34 +0300

    creating services

--- Games/src/app/game/game.component.ts
+++ Games/src/app/game/game.component.ts
@@ -7,16 +7,17 @@ import { Game } from '../game';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
+  games: any;
 
-  games:Game[]=[
-    new Game(1,'FIFA 22','Love the Graphics',new Date(2020,0,18)),
-    new Game(2,'Pes 22','The speed is awesome',new Date(2021,5,18)),
-    new Game(3,'Dream League','Compatible in my device',new Date(2022,8,29))
-    // {id:1,name:'FIFA 22',description:'Love the graphics'},
-    // {id:2,name:'PES 22',description:'The speed is awesome while using it'},
-    // {id:3,name:'Dream League 2022',description:'Compatible in my device'}
+  // games:Game[]=[
+  //   new Game(1,'FIFA 22','Love the Graphics',new Date(2020,0,18)),
+  //   new Game(2,'Pes 22','The speed is awesome',new Date(2021,5,18)),
+  //   new Game(3,'Dream League','Compatible in my device',new Date(2022,8,29))
+  //   // {id:1,name:'FIFA 22',description:'Love the graphics'},
+  //   // {id:2,name:'PES 22',description:'The speed is awesome while using it'},
+  //   // {id:3,name:'Dream League 2022',description:'Compatible in my device'}
 
-  ]
+  // ]
 toggleDetail(index: number){
 this.games[index].showDescription=!this.games[index].showDescription;
 }
