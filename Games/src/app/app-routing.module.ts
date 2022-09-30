8ba1bead125aef92e6cdbb088fb33da704d3fd28
SayiaFelix <sayiafelix18@gmail.com>
2022-09-30 09:35:36 +0300

    adding routing

--- Games/src/app/app-routing.module.ts
+++ Games/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'games', component: GameComponent},
+  { path: 'about', component: AboutComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
