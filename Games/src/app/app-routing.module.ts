515bcfd7ce2a6fa130968c68e7d4ee784581f736
SayiaFelix <sayiafelix18@gmail.com>
2022-10-05 09:35:33 +0300

    adding quote routing

--- Games/src/app/app-routing.module.ts
+++ Games/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { AboutComponent } from './about/about.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 const routes: Routes = [
   { path: 'games', component: GameComponent},
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent},
+  { path: 'about', component: QuotesComponent}
 
 ];
 
