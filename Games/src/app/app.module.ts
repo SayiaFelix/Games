db118b8355004161f3b29a7b60126735db8eac22
SayiaFelix <sayiafelix18@gmail.com>
2022-10-05 09:21:18 +0300

    fixing bug

--- Games/src/app/app.module.ts
+++ Games/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule ,routingComponents} from './app-routing.module';
+import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { GamedetailComponent } from './gamedetail/gamedetail.component';
